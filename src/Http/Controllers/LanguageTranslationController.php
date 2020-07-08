f058cc34e554ecb20d2671ec30714ff70395c60d
gardocki <marotta.anto@gmail.com>
2020-07-08 16:04:24 +0200

    Update LanguageTranslationController.php
    bug fix on eloquent collection: converted to base support collection

--- src/Http/Controllers/LanguageTranslationController.php
+++ src/Http/Controllers/LanguageTranslationController.php
@@ -27,7 +27,8 @@ class LanguageTranslationController extends Controller
         }
 
         $languages = $this->translation->allLanguages();
-        $groups = $this->translation->getGroupsFor(config('app.locale'))->merge('single');
+        $groups = $this->translation->getGroupsFor(config('app.locale'))->toBase();
+        $groups = $groups->merge(['single']);
         $translations = $this->translation->filterTranslationsFor($language, $request->get('filter'));
 
         if ($request->has('group') && $request->get('group')) {
